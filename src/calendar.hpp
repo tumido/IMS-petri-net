8f206537839013f8358eb0ba20b49c8ec152a810
Tomas Coufal <tcoufal@redhat.com>
2015-12-08 01:52:36 +0100

    [structure] added Calendar, Event

--- src/calendar.hpp
+++ src/calendar.hpp
@@ -1,14 +1,31 @@
 /**
  * @file calendar.hpp
- * @author Albert Uchytil
+ * @author Albert Uchytil, Tomas Coufal
  * @brief Calendar of actions for simulation
  */
 #ifndef __CALENDAR_HPP__
 #define __CALENDAR_HPP__
 
+#include <vector>
+#include <set>
+#include "event.hpp"
+
+struct compare {
+    bool operator() (const Event * first, const Event * second) const {
+        return first->GetTime() < second->GetTime();
+    }
+};
+
 class Calendar {
 private:
+    std::multiset<Event *, compare> list;
 public:
+    Calendar();
+    ~Calendar();
+    void AppendEvent(Event *);
+    Event * GetEvent();
+    void RemoveEvent();
+    bool Empty();
 };
 
 #endif // __CALENDAR_HPP__
--- src/event.hpp
+++ src/event.hpp
@@ -1,14 +1,31 @@
 /**
  * @file event.hpp
- * @author Albert Uchytil
+ * @author Albert Uchytil, Tomas Coufal
  * @brief Event class which specifies calendar actions
  */
 #ifndef __EVENT_HPP__
 #define __EVENT_HPP__
 
+#include <vector>
+#include "transition.hpp"
+#include "token.hpp"
+
 class Event {
 private:
+    double time;
+    Transition * transition;
+    std::vector<Token *> tokens;
 public:
+    Event(double , Transition * );
+    ~Event();
+    double GetTime() const { return this->time; };
+    Transition * GetTransitionPtr();
+    void AddToken(Token *);
+    Token * PopToken();
+    bool operator < (const Event& ref) const
+    {
+        return (this->time < ref.time);
+    }
 };
 
 #endif // __EVENT_HPP__
